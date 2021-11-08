d4a5e361db1bdbbb43dab4ca7bdb1c482b213990
rockpell <aythffk@gmail.com>
2021-11-08 22:41:18 +0900

    [feat] 게시글 리스트 api, image resize 적용
    - 게시글 리스트를 가져오는 api에서 반환하는 image의 url을 resized된 이미지 url을 내려주도록

--- src/util/index.ts
+++ src/util/index.ts
@@ -9,4 +9,21 @@ const verifyToken = (token: string) => {
   return verify(token, process.env.JWT_SECRET) as JwtPayload;
 };
 
-export { createToken, verifyToken };
+const getResizedImage = (url: string) => {
+  if (!url) return null;
+
+  const urlList = url.split('/');
+
+  if (urlList.length < 3) return null;
+
+  const imageName = urlList[urlList.length - 1];
+  const region = process.env.AWS_REGION;
+  const uploadBucket = process.env.UPLOAD_BUCKET;
+
+  if (!region) return null;
+  if (!uploadBucket) return null;
+
+  return `https://${uploadBucket}-resize.s3.${region}.amazonaws.com/resized-${imageName}`;
+};
+
+export { createToken, verifyToken, getResizedImage };
