f3cbfc4198e2f762035856a588e7f96ee42a5993
rockpell <aythffk@gmail.com>
2021-11-29 23:24:42 +0900

    [feat] update post api 추가

--- src/db/post.ts
+++ src/db/post.ts
@@ -1,5 +1,5 @@
+import { IUpdatePost, IPostBoardList, IPost } from './../interface/interface';
 import pool from '.';
-import { IPostBoardList, IPost } from '../interface/interface';
 import { TPOST_STATUS } from '../interface/types';
 
 const PAGE_SIZE = 5;
@@ -104,6 +104,67 @@ export const writePost = async (post: IPost): Promise<number> => {
     console.log(err);
     return err;
   }
+};
+
+export const updatePost = async (post: IUpdatePost): Promise<void> => {
+  const option = JSON.stringify(post.option);
+  await pool.query(
+    `UPDATE POST SET
+      campus_id = $1,
+      title = $2,
+      contact = $3,
+      deposit = $4,
+      monthly_rent = $5,
+      service_fee = $6,
+      electricity = $7,
+      water = $8,
+      gas = $9,
+      contract_expire_date = $10,
+      move_in_date = $11,
+      address = $12,
+      address_detail = $13,
+      is_address_visible = $14,
+      total_floor = $15,
+      current_floor = $16,
+      building_type = $17,
+      room_type = $18,
+      window_side = $19,
+      walking_time = $20,
+      bus_time = $21,
+      content = $22,
+      post_status = $23,
+      option = $24
+    WHERE
+      id = $25
+    `,
+    [
+      post.campus_id,
+      post.title,
+      post.contact,
+      post.deposit,
+      post.monthly_rent,
+      post.service_fee,
+      post.electricity,
+      post.water,
+      post.gas,
+      post.contract_expire_date,
+      post.move_in_date,
+      post.address,
+      post.address_detail,
+      post.is_address_visible,
+      post.total_floor,
+      post.current_floor,
+      post.building_type,
+      post.room_type,
+      post.window_side,
+      post.walking_time,
+      post.bus_time,
+      post.content,
+      post.post_status,
+      option,
+      post.id,
+    ],
+  );
 };
 
 export const deletePost = async (postId: number): Promise<boolean> => {
--- src/route/post/write.ts
+++ src/route/post/write.ts
@@ -1,3 +1,4 @@
+import { IUpdatePost } from './../../interface/interface';
 import express from 'express';
 const router = express.Router();
 import * as logic_post from '../../logic/post';
@@ -39,6 +40,41 @@ router.post('/', async (req: any, res: any) => {
 
   //TODO - postId를 사용해서 이미지 업로드
   res.status(200).json({ post_id: postId, message: 'SUCCESS' });
+});
+
+router.put('/:id', async (req: any, res: any) => {
+  const id = req.params.id as number;
+  const post: IUpdatePost = {
+    id,
+    campus_id: req.body.campus_id,
+    title: req.body.title,
+    contact: req.body.contact,
+    deposit: req.body.deposit,
+    monthly_rent: req.body.monthly_rent,
+    service_fee: req.body.service_fee,
+    electricity: req.body.electricity,
+    water: req.body.water,
+    gas: req.body.gas,
+    contract_expire_date: req.body.contract_expire_date,
+    move_in_date: req.body.move_in_date,
+    address: req.body.address,
+    is_address_visible: req.body.is_address_visible,
+    address_detail: req.body.address_detail,
+    total_floor: req.body.total_floor,
+    current_floor: req.body.current_floor,
+    building_type: req.body.building_type,
+    room_type: req.body.room_type,
+    window_side: req.body.window_side,
+    walking_time: req.body.walking_time,
+    bus_time: req.body.bus_time,
+    content: req.body.content,
+    post_status: req.body.post_status,
+    option: req.body.option,
+    images: req.body.images,
+  };
+  await logic_post.updatePost(post);
+
+  res.status(200).json({ message: 'SUCCESS' });
 });
 
 export default router;
