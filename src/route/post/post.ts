45d94cb5b2c1116ab34870c955819492f27a52f4
rockpell <aythffk@gmail.com>
2022-01-03 21:21:53 +0900

    chore: 일관된 코드 스타일로 통일

--- src/route/post/post.ts
+++ src/route/post/post.ts
@@ -26,9 +26,7 @@ router.delete('/:id', async (req: any, res: any, next) => {
 
     const isSuccess = await logic_post.deletePost(postId);
 
-    if (!isSuccess) {
-      throw new CustomError('post가 존재하지 않습니다.', 204);
-    }
+    if (!isSuccess) throw new CustomError('post가 존재하지 않습니다.', 204);
 
     res.status(200).send();
   } catch (err) {
